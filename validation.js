a8a2324d2d50d61628968f2eb75f19ea2a778814
patrick <patrick204nqh@gmail.com>
2020-06-18 17:04:36 +0700

    testing a regex pattern

--- validation.js
+++ validation.js
@@ -1,3 +1,21 @@
+const inputs = document.querySelectorAll('input');
+
 const patterns = {
   telephone: /^\d{11}$/,
 };
+
+// validation function
+function validate(field, regex) {
+  if (regex.test(field.value)) {
+    field.className = 'valid';
+  } else {
+    field.className = 'invalid';
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener('keyup', (e) => {
+    // console.log(e.target.attributes.name.value);
+    validate(e.target, patterns[e.target.attributes.name.value]);
+  });
+});
